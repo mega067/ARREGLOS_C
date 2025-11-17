820f0eb435a5c2be08ff3efb69530d5da40fa39f
Mega067L <77284535+mega067@users.noreply.github.com>
2025-11-17 17:39:08 +0000

    a)

--- practicas/Arreglos/cereales.c
+++ practicas/Arreglos/cereales.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
-{
-    printf("Hola");
-    printf("\n");
+{   
+    int arreglo[12] = {1, 5, 8, 9, 4, 7, 9, 8, 1, 6, 3, 2};
+    int C ;
+    int S = 0 ;
+    int R = 0 ;
+     
+    for (C = 0; C < 12; C++)
+    {
+        printf ("%d\n",arreglo[C]);
+        S = S + arreglo[C];
+    }
+    printf("SUMA\n");
+    printf ("%d\n",S);
+    
+    R = S / 12 ;
+    printf("a) Promedio anul en toneladas\n");
+    printf("%d\n",R);
+    
     return 0;
 }
