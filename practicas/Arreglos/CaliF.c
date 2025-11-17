1677019b2f504f24bf7dab5d0532453adfdd319f
Mega067L <77284535+mega067@users.noreply.github.com>
2025-11-17 23:58:32 +0000

    Cal

--- practicas/Arreglos/CaliF.c
+++ practicas/Arreglos/CaliF.c
@@ -7,6 +7,8 @@ int main(int argc, char const *argv[])
     int Arre [1000];
     int Pro;
     int A_pro = 0;
+    float P_A = 0;
+    float P_Na = 0;
     int No_pro = 0;
     int i;
     int C;
@@ -40,6 +42,12 @@ printf("\nPromedio general del grupo:  %d\n",Pro);
 
 printf("\nNumero de alumnos Aprobados: %d\n",A_pro);
 printf("\nNumero de alumnos No aprobados: %d\n",No_pro);
+
+P_A = ((float)A_pro / N)*100;
+P_Na = ((float)No_pro / N)*100;
+
+printf("\nPorcentaje de alumnos aprobados: %f%%\n",P_A); 
+printf("\nPorcentaje de alumnos No aprobados: %f%%\n",P_Na); 
 
     return 0;
 }
